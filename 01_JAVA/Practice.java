d58f67c944bc50619ad5b62ed168ef444dc10dc5
Krushna Chandra Nanda <139350558+Krushna-Nanda@users.noreply.github.com>
2024-01-16 22:21:18 +0530

    temporary file

---
+++ 01_JAVA/Practice.java
@@ -0,0 +1,14 @@
+public class Practice {
+    public static void main(String[] args) { 
+        System.out.println("Hello World!");
+    }
+
+    /* in java what happen is that everthing is inside a class , and the class name should be the same as the file name , why because its a convention . AND main method is the entry point of the program , but in classes what happen is we cant access anything inside the class without creating an object , so we need to create an object to access the main method , but in java we have a keyword called static , so if we use static keyword in front of the main method , then we can access the main method without creating an object , and the main method should be public , because we need to access it from outside the class , and the main method should be void , because we dont need to return anything from the main method , and the main method should have a parameter called String[] args , because we need to pass arguments to the main method , and the main method should be static because we need to access it without creating an object , and the main method should be public because we need to access it from outside the class */
+    
+    
+    
+    
+    
+    
+    
+}
