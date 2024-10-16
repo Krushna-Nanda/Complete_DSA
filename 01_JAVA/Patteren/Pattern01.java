e836277c81ef232177c50f75bfbc7a48a3112d83
Krushna Chandra Nanda <139350558+Krushna-Nanda@users.noreply.github.com>
2024-10-16 12:27:08 +0530

    added patteren 1

---
+++ 01_JAVA/Patteren/Pattern01.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Pattern01 {
+    public static void main(String[] args) {
+        /*
+         * Given an integer n. You need to recreate the pattern given below for any
+         * value of N. Let's say for N = 5, the pattern should look like as below:
+         *****
+         *****
+         *****
+         *****
+         ***** 
+         */
+        System.out.println("enter the no :--");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        pattern(n);
+
+    }
+
+    public static void pattern(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
